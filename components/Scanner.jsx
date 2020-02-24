4c1ffcabb6af2f64cb4d6f10af6c7966ed355466
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2020-02-23 21:10:41 -0500

    remove rando

--- components/Scanner.jsx
+++ components/Scanner.jsx
@@ -37,8 +37,6 @@ export default function Scanner({navigation}) {
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
-
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
 }
